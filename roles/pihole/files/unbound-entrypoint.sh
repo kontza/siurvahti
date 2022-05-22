4f1b2601868fa979bf37619f5cc63cb55629ced4
Juha Ruotsalainen <kontza@gmail.com>
2022-05-22 15:17:44 +0300

    Trying to get unbound entry point working.

--- roles/pihole/files/unbound-entrypoint.sh
+++ roles/pihole/files/unbound-entrypoint.sh
@@ -63,7 +63,4 @@ server:
 EOT
 fi
 
-mkdir -p -m 700 /opt/unbound/etc/unbound/var && \
-/opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key
-
 exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf

8d3964b955470a840d5d0dc881feadf45a9beb1b
Juha Ruotsalainen <juha.ruotsalainen@iki.fi>
2022-05-29 15:51:48 +0300

    For some reason using volumes prevents pihole+unbound working propely.

--- create-pihole.sh
+++ create-pihole.sh
@@ -2,8 +2,10 @@ podman create \
 	--cap-add CAP_NET_RAW \
 	--cap-add CAP_NET_ADMIN \
 	--cap-add CAP_SYS_NICE \
-	--pod pod-pihole-unbound \
 	--env TZ='Europe/Helsinki' \
-	--env WEBPASSWORD='Atlas-Breath-Anthology-Dingy-Uncombed-Illusive' \
-	--name container-pihole \
+	--env WEBPASSWORD='e_inutile' \
+	--replace \
+	--name pihole \
 	docker.io/pihole/pihole
+#       --volume ./pihole/:/etc/pihole:z \
+#       --volume ./dnsmasq/:/etc/dnsmasq.d:z \
--- create-pod.sh
+++ create-pod.sh
@@ -1,8 +1,8 @@
 podman pod create \
-	--dns='127.0.0.1'\
-	--dns='192.168.1.43' \
-	--hostname=pihole-unbound \
-	--name=pod-pihole-unbound \
+	--dns '127.0.0.1'\
+	--dns '192.168.1.43' \
+	--replace \
+	--name pod \
 	--publish=10053:53/udp \
 	--publish=10053:53/tcp \
 	--publish=10080:80/tcp
